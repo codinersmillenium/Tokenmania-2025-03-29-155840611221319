d36037e984aa5960f67770e2105923ee9ac9adc3
ICP.Ninja <noreply@icp.ninja>
2025-03-29 22:58:58 +0700

    ICP.Ninja export

---
+++ frontend/src/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Footer = () => {
+    return (
+        <footer className="bg-gray-700">
+            <div className="container__main text-center py-8">
+                <marquee>
+                    <span className="text-xl text-sky-500 font-semibold">The Best Web3 Marketplace </span>
+                </marquee>
+            </div>
+        </footer>
+    );
+};
+export default Footer;
